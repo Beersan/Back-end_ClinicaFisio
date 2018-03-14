cf24cc4a6286080dd909c6a83198e40cbb65f478
Colegio de Aplicacao Lab <ca.lab@uniarp.edu.br>
2018-03-13 21:28:20 -0300

    Conexão criada

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	
+	const { Client } = require('pg');
+
+	const client = new Client({
+	  connectionString: process.env.DATABASE_URL,
+	  ssl: true,
+	});
+
+	client.connect();
+
+	client.query('SELECT * from teste;', (err, res) => {
+	  if (err) throw err;
+	  for (let row of res.rows) {
+		console.log(JSON.stringify(row));
+	  }
+	  client.end();
+	});	
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
