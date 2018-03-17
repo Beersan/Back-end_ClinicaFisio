b16590651ff9bb9a91a484dacefc67fefbb27a8a
DANIEL BRESSAN <015613@uniarp.edu.br>
2018-03-17 11:33:59 -0300

    nodemon e um JSON

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
+
 	const { Client } = require('pg');
 
 	const client = new Client({
@@ -19,9 +22,29 @@ router.get('/', function(req, res, next) {
 		console.log(JSON.stringify(row));
 	  }
 	  client.end();
-	});	
+	});
 	res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/precadastro', function (req, res, next) {
+	 response = {
+		"nome_completo": "koe Silva",
+ 		"rg": "5.258.868",
+ 		"cpf": "077.066.859-55",
+ 		"data_nasc": "25/12/1994",
+ 		"renda_mensal": "5.000,00",
+ 		"endereco": "Ruasdjaslidhalsih",
+ 		"numero": "100",
+ 		"bairro": "Vila kurtz",
+ 		"cidade": "Cacador",
+ 		"contato1": "49988155536",
+ 		"contato2": "49984155536",
+ 		"encaminhamento_med": "sim",
+ 		"area_especialidade": "coluna"
+ };
+ console.log(response);
+ res.end(JSON.stringify(response));
+});
 
+
+module.exports = router;
