fa5d244fdd8ac9b1df5260d0acdd1c1790872ffc
DANIEL BRESSAN <015613@uniarp.edu.br>
2018-03-27 20:21:34 -0300

    Correção especialidade

--- routes/especialidade.js
+++ routes/especialidade.js
@@ -10,20 +10,27 @@ const client = new Client({
 
 router.post('/cadastrar', function(req, res){
 
+  const client = new Client({
+    connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
+    ssl: true,
+  });
+
   const data = {descricaoEspecialidade: req.body.descricaoEspecialidade};
 
+  console.log('teste' + JSON.stringify(data));
+
   client.connect((err, client, done) => {
-    /*if(err){
+    if(err){
       done();
       console.log(err);
       return res.status(500).json({success: false, data: err});
-    }*/
+    }
     console.log(data.descricaoEspecialidade);
 
     client.query("INSERT INTO especialidade(descricaoespecialidade) values($1)", [data.descricaoEspecialidade]);
 
     res.send({
-      message: 'Agora vai da certo'
+      message: 'OK ok'
     });
     /*query.on('row', (row) => {
         results.push(row);
