3a58882e7b19ec642c8f8a0d842eaef75d6b64f8
Daniel Bressan <danielbressan125@gmail.com>
2018-04-01 20:46:14 -0300

    Despois termino

--- routes/especialidade.js
+++ routes/especialidade.js
@@ -1,78 +1,75 @@
 var express = require('express');
 var router = express.Router();
-const results = [];
-
 const { Client } = require('pg');
-const client = new Client({
-  connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
-  ssl: true,
-});
 
 router.post('/cadastrar', function(req, res){
-
   const client = new Client({
     connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
     ssl: true,
   });
-
   const data = {descricaoEspecialidade: req.body.descricaoEspecialidade};
-
-  console.log('teste' + JSON.stringify(data));
 
   client.connect((err, client, done) => {
     if(err){
-      done();
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }
-    console.log(data.descricaoEspecialidade);
-
     client.query("INSERT INTO especialidade(descricaoespecialidade) values($1)", [data.descricaoEspecialidade]);
-
     res.send({
-      message: 'OK ok'
+      message: 'ok'
     });
-    /*query.on('row', (row) => {
-        results.push(row);
-    });*/
-
-    // query.on('end', ()=>{
-    //   done();
-    //   return res.json(results);
-    // });
   });
-/*
+});
 
 router.get('/listar', function(req, res, next) {
-
+  const client = new Client({
+    connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
+    ssl: true,
+  });
   client.connect();
-
-  client.query('SELECT * from estagiario;', (err, response) => {
+  client.query('SELECT * from especialidade order by descricaoespecialidade;', (err, response) => {
     if (err) throw err;
     res.send(response.rows);
-
-    client.end();
-
   });
+});
 
-*/
+
+router.post('/excluir', function(req, res){
+  const client = new Client({
+    connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
+    ssl: true,
+  });
+  const data = {codigoespecialidade: req.body.codigoEspecialidade};
+
+  client.connect((err, client, done) => {
+    if(err){
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    client.query("DELETE FROM especialidade WHERE codigoespecialidade = $1", [data.codigoEspecialidade]);
+    res.send({
+      message: 'ok'
+    });
+  });
+});
+
+router.post('/editar', function(req, res){
+  const client = new Client({
+    connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
+    ssl: true,
+  });
+  const data = {nome: req.body.descricaoEspecialidade};
+
+  client.connect((err, client, done) => {
+    if(err){
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    client.query("update especialidade set descricaoespecialidade = ($1) where codigoespecialidade = ($5)", [data.descricaoEspecialidade, data.codigoEspecialidade]);
+    res.send({
+      message: 'ok'
+    });
+  });
 });
 
 module.exports = router;
-
-
-/*
-    res.rows.forEach(row=>{
-      console.log(row);
-    });
-    await client.end();
-    query.on('row', (row) => {
-      results.push(row);
-      console.log("1");
-    });
-    console.log("2");
-    query.on('end', ()=>{
-      done();
-      return res.json(results);
-      console.log("3");
-    });*/
