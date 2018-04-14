d79b1b02a5a348a602d4b2f8dd68de3d90e5a499
thaisdv <thaisdovall494@hotmail.com>
2018-04-14 14:37:08 -0300

    finalizado crud semestre

--- routes/semestre.js
+++ routes/semestre.js
@@ -11,7 +11,6 @@ router.post('/cadastrar', function(req, res){
   
   client.connect((err, client, done) => {
     if(err){
-      console.log(err);
       return res.status(500).json({success: false, data: err});
     }    
     client.query("INSERT INTO semestre(nomesemestre, datainiciosemestre, datafimsemestre) values($1, $2, $3)", [data.nome, data.dataInicio, data.dataFim]);         
@@ -21,32 +20,30 @@ router.post('/cadastrar', function(req, res){
   }); 
 });
 
-/*router.get('/listar', function(req, res, next) {
+router.get('/listar', function(req, res, next) {
   const client = new Client({
     connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
     ssl: true,
   });
   client.connect();  
-  client.query('SELECT * from estagiario order by nomeestagiario;', (err, response) => {
+  client.query('SELECT * from semestre order by nomesemestre;', (err, response) => {
     if (err) throw err;
     res.send(response.rows);
   });          
-});
-  
+});  
 
 router.post('/excluir', function(req, res){ 
   const client = new Client({
     connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
     ssl: true,
   });
-  const data = {idEstagiario: req.body.idEstagiario};
+  const data = {idSemestre: req.body.idSemestre};
   
   client.connect((err, client, done) => {
     if(err){
-      console.log(err);
       return res.status(500).json({success: false, data: err});
     }    
-    client.query("DELETE FROM estagiario WHERE idestagiario = $1", [data.idEstagiario]);         
+    client.query("DELETE FROM semestre WHERE idsemestre = $1", [data.idSemestre]);         
     res.send({
       message: 'ok'
     });
@@ -58,18 +55,17 @@ router.post('/editar', function(req, res){
     connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
     ssl: true,
   });
-  const data = {nome: req.body.nomeEstagiario, matricula: req.body.numeroMatricula, email: req.body.email, telefone: req.body.telefone, idEstagiario: req.body.idEstagiario};
+  const data = {nome: req.body.nomeSemestre, dataInicio: req.body.dataInicioSemestre, dataFim: req.body.dataFimSemestre, idSemestre: req.body.idSemestre};
   
   client.connect((err, client, done) => {
     if(err){
-      console.log(err);
       return res.status(500).json({success: false, data: err});
     }    
-    client.query("update estagiario set nomeestagiario = ($1), matriculaestagiario = ($2), emailestagiario = ($3), telefoneestagiario = ($4) where idestagiario = ($5)", [data.nome, data.matricula, data.email, data.telefone, data.idEstagiario]);         
+    client.query("update semestre set nomesemestre = ($1), datainiciosemestre = ($2), datafimsemestre = ($3) where idsemestre = ($4)", [data.nome, data.dataInicio, data.dataFim, data.idSemestre]);         
     res.send({
       message: 'ok'
     });
   }); 
-});*/
+});
 
 module.exports = router;
