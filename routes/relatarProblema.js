19fe206efc015232171a31c50dde03d47ca26a66
DANIEL BRESSAN <015613@uniarp.edu.br>
2018-04-14 11:00:01 -0300

    Pronto

--- routes/relatarProblema.js
+++ routes/relatarProblema.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const { Client } = require('pg');
 var nodemailer = require('nodemailer');
 
 router.post('/relatarProblema', function(req, res){
-
+  const client = new Client({
+    connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
+    ssl: true,
+  });
+  const data = {assuntoProblema: req.body.assuntoProblema, descricaoProblema: req.body.descricaoProblema};
+  
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -12,12 +18,11 @@ router.post('/relatarProblema', function(req, res){
     },
     tls: { rejectUnauthorized: false }
   });
-  const data = {descricaoProblema: req.body.descricaoProblema};
   
   var mailOptions = {
     from: 'no.reply.fisio@gmail.com',
     to: 'daniel_bressan125@hotmail.com',
-    subject: 'Reclamação!!',
+    subject: data.assuntoProblema,
     text: data.descricaoProblema
   };
   
@@ -28,8 +33,16 @@ router.post('/relatarProblema', function(req, res){
       console.log('Email enviado: ' + info.response);
     }
   });
-  res.send({
-    message: 'ok'
+
+  client.connect((err, client, done) => {
+    if(err){
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }    
+    client.query("INSERT INTO problema(assuntoproblema, descricaoproblema) values($1, $2)", [data.assuntoProblema, data.descricaoProblema]);
+    res.send({
+      message: 'ok'
+    });
   });
 });
 
