23c6a41204b2cb0bd867a3a223f794d7cb444217
thaisdv <thaisdovall494@hotmail.com>
2018-04-17 21:31:32 -0300

    inclusão semestre grupo

--- routes/grupo.js
+++ routes/grupo.js
@@ -7,14 +7,14 @@ router.post('/cadastrar', function(req, res){
     connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
     ssl: true,
   });
-  const data = {descricaoGrupo: req.body.descricao};
+  const data = {descricaoGrupo: req.body.descricao, semestre: req.body.semestre};
   
   client.connect((err, client, done) => {
     if(err){
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }    
-    client.query("INSERT INTO grupo(descricaogrupo) values($1)", [data.descricaoGrupo]);         
+    client.query("INSERT INTO grupo(descricaogrupo, idsemestre) values($1, $2)", [data.descricaoGrupo, data.semestre]);         
     res.send({
       message: 'ok'
     });
@@ -27,7 +27,7 @@ router.get('/listar', function(req, res, next) {
       ssl: true,
     });
     client.connect();  
-    client.query('SELECT * from grupo order by descricaogrupo;', (err, response) => {
+    client.query('SELECT grupo.descricaogrupo, semestre.nomesemestre, grupo.idsemestre, grupo.idgrupo from grupo inner join semestre ON semestre.idsemestre = grupo.idsemestre order by descricaogrupo;', (err, response) => {
       if (err) throw err;
       res.send(response.rows);
     });          
@@ -58,18 +58,31 @@ router.get('/listar', function(req, res, next) {
       connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
       ssl: true,
     });
-    const data = {descricaoGrupo: req.body.descricao, idGrupo: req.body.idGrupo};
+    const data = {descricaoGrupo: req.body.descricao, idGrupo: req.body.idGrupo, semestre: req.body.semestre};
     
     client.connect((err, client, done) => {
       if(err){
         console.log(err);
         return res.status(500).json({success: false, data: err});
       }    
-      client.query("update grupo set descricaogrupo = ($1) where idgrupo = ($2)", [data.descricaoGrupo, data.idGrupo]);         
+      console.log(data.semestre);
+      client.query("update grupo set descricaogrupo = ($1), idsemestre = ($2) where idgrupo = ($3)", [data.descricaoGrupo, data.semestre, data.idGrupo]);         
       res.send({
         message: 'ok'
       });
     }); 
+  });
+
+  router.get('/listarsemestre', function(req, res, next) {
+    const client = new Client({
+      connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
+      ssl: true,
+    });
+    client.connect();  
+    client.query('SELECT * from semestre order by nomesemestre;', (err, response) => {
+      if (err) throw err;
+      res.send(response.rows);
+    });          
   });
 
   module.exports = router;
