9278bfc004be7bef669b51c72998c5f16722a25e
Gabriel <gabriel_14_oliveira@hotmail.com>
2018-05-16 23:52:45 -0300

    Fila espera

--- routes/filaEspera.js
+++ routes/filaEspera.js
@@ -2,13 +2,23 @@ var express = require('express');
 var router = express.Router();
 var client = require('./dbConnection');
 
+router.post('/retornarArquivosPacientes', function(req, res, next) {    
+  const data = {idpaciente: req.body.idpaciente}; 
+  client.query("SELECT PA.idpaciente, PA.arquivo "
+            + " FROM pacientearquivos PA "
+            + "     WHERE PA.idpaciente = $1" 
+            + "     AND PA.arquivo IS NOT NULL", [data.idpaciente], (err, response) => {
+    if (err) throw err;
+    res.send(response.rows);
+  });          
+});
+
 router.get('/listarPacientesFila', function(req, res, next) {
   client.query("SELECT P.bairropaciente, P.cidadepaciente, P.codigoespecialidade, P.contato1paciente, "
                 + " CASE WHEN P.contato2paciente IS NULL THEN 'Não informado' ELSE P.contato2paciente END AS telefone2 , "
                 + " P.cpfpaciente, P.datanascpaciente, P.encmedicopaciente, P.encmedpaciente, "
                 + " P.enderecopaciente,	P.idpaciente,	P.nomepaciente,	P.numeropaciente, "
                 + " P.observacoespaciente,	P.rendapaciente,	P.rgpaciente, E.descricaoespecialidade, "
-                + " CASE WHEN encmedpaciente IS NULL THEN 'none' ELSE 'initial' END AS classeenc,"
                 + " CASE WHEN ES.nomeestagiario IS NULL THEN 'Não vinculado' ELSE ES.nomeestagiario END AS nomeEstagiario "
                 + " FROM paciente P "
                 + "   INNER JOIN especialidade E ON E.codigoespecialidade = P.codigoespecialidade" 
@@ -37,13 +47,14 @@ router.get('/listarEstagiariosFila', function(req, res, next) {
     });       
 });
 
-router.post('/vincularPacienteEstagiario', function(req, res){   
-  const data = {paciente: req.body.idpaciente, estagiario: req.body.idestagiario};
-  
-  client.query("INSERT INTO estagiariopacientes (idpaciente,idestagiario)values($1, $2)", [data.paciente, data.estagiario]);         
-    res.send({
-      message: 'ok'
-    });
+router.post('/gravarAnexosPaciente', function(req, res){   
+  const data = {anexos: req.body.anexos, idpaciente: req.body.idpaciente};
    
+  client.query("DELETE FROM pacientearquivos WHERE idpaciente = $1", [data.idpaciente]);         
+  for (i = 0; i <= data.anexos.length; i++){
+    client.query("INSERT INTO pacientearquivos(idpaciente,arquivo) values($1, $2)", [data.idpaciente, data.anexos[i]]);         
+  }       
+  res.send({message: 'ok'});   
 });
+
 module.exports = router;
