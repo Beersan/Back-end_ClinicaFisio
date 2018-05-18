9569b8664c5f465eb24f161ad30e21c2e0062bc4
Gabriel <gabriel_14_oliveira@hotmail.com>
2018-05-18 00:05:52 -0300

    professor

--- routes/agendaProfessor.js
+++ routes/agendaProfessor.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-const { Client } = require('pg');
-const client = new Client({
-  connectionString: 'postgres://avzgogfkefojwd:98673260249a154f7aec7832ad4e843fe04bf1debc600e98f04b82c2da2c64ea@ec2-54-221-220-59.compute-1.amazonaws.com:5432/dcasactg6t0691',
-  ssl: true,
-});
-client.connect(); 
+var client = require('./dbConnection');
 
 router.get('/listarProfessor', function (req, res){
     client.query("select idprofessor, nomeprofessor from professor, especialidade where professor.codigoespecialidade = especialidade.codigoespecialidade and ativo = 1 order by nomeprofessor"  , (err, response) => {if (err) throw err;
@@ -32,22 +26,18 @@ router.get('/listarHoraFim', function (req, res){
     });          
 });
 
-router.get('/inserirAgenda', function (req, res){
+router.post('/inserirAgenda', function (req, res){
     const data = {
-        nomeProfessor : req.body.nomeProfessor,
-        descricaoDiaSemana : req.body.descricaoDiaSemana,
-        descricaoHoraInicio : req.body.descricaoHoraInicio,
-        descricaoHoraFim : req.body.descricaoHoraFim
+        professor : req.body.idprofessor,
+        dia : req.body.diaSemana,
+        horaInicio : req.body.horaInicio,
+        horaFim : req.body.horaFim
     };
-    //console.log("valores " + professor, diaSemana, horainicio, horafim)
-        client.query("insert into agendaprofessor (idprofessor, iddiasemana, idhorainicio, idhorafim) values (" + 
-        "(select idprofessor from professor where nomeprofessor = ($1) and ativo = 1), " +
-        "(select iddiasemana from diasemana where descricaosemana = ($2)), " +
-        "(select idhorainicio from horainicio where descricaohorainicio = ($3)), " +
-        "(select idhorafim from horafim where descricaohorafim = ($4)))"
-        , [data.nomeProfessor, data.descricaoDiaSemana, data.descricaoHoraIniciohorainicio, data.descricaoHoraFim]);       
-        res.send({
-        message: 'ok' + data.nomeProfessor
-    });
+    console.log(data);
+    for (i = 0; i <= data.dia.length; i++){
+      client.query("INSERT INTO agendaprofessor (idprofessor, iddiasemana, idhorainicio, idhorafim) VALUES ($1,$2,$3,$4)", 
+      [data.professor, data.dia[i], data.horaInicio[i], data.horaFim[i]]);
+    }
+    res.send({message: 'ok'});
 });
 module.exports = router;
