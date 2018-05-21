b26eeab2cbc0e4bfe001f00799a73a7a79215c4d
dobkovski <jdobkovski@gmail.com>
2018-05-21 09:56:58 -0300

    Agenda

--- routes/agendaProfessor.js
+++ routes/agendaProfessor.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var client = require('./dbConnection');
+var diaSemanaString;
+var diaSemanaArray;
+var descricaoHoraInicioArray;
+var descricaoHoraFimArray;
 
 router.get('/listarProfessor', function (req, res){
     client.query("select idprofessor, nomeprofessor from professor, especialidade where professor.codigoespecialidade = especialidade.codigoespecialidade and ativo = 1 order by nomeprofessor"  , (err, response) => {if (err) throw err;
@@ -28,22 +32,38 @@ router.get('/listarHoraFim', function (req, res){
 
 router.post('/inserirAgenda', function (req, res){
     const data = {
-        nomeProfessor : req.body.idprofessor,
+        idprofessor : req.body.idprofessor,
         descricaoDiaSemana : req.body.diaSemana,
         descricaoHoraInicio : req.body.horaInicio,
         descricaoHoraFim : req.body.horaFim
     };
-    console.log("valores " + data.nomeProfessor, data.descricaoDiaSemana, data.descricaoHoraInicio, data.descricaoHoraFim)
-    for (i = 0; i <= data.descricaoDiaSemana.length; i++){    
-        client.query("insert into agendaprofessor (idprofessor, iddiasemana, idhorainicio, idhorafim) values (" + 
-            "(select idprofessor from professor where nomeprofessor = ($1) and ativo = 1), " +
-            "(select iddiasemana from diasemana where descricaosemana = $2)," +
+    //console.log(data.descricaoDiaSemana[1]);
+    //console.log("tamanho = " + data.descricaoDiaSemana.length);
+
+    for(i = 0; i <= data.descricaoDiaSemana.length; i++){
+
+        if(data.descricaoDiaSemana[i] == 'Segunda-Feira'){
+            diaSemanaString = 1;
+        }else if(data.descricaoDiaSemana[i] == 'Terça-Feira'){
+            diaSemanaString = 2;
+        }else if(data.descricaoDiaSemana[i] == 'Quarta-Feira'){
+            diaSemanaString = 3;
+        }else if(data.descricaoDiaSemana[i] == 'Quinta-Feira'){
+            diaSemanaString = 4;
+        }else if(data.descricaoDiaSemana[i] == 'Sexta-Feira'){
+            diaSemanaString = 5;
+        }
+
+            console.log(data.descricaoDiaSemana[i]);
+            console.log(data.descricaoDiaSemana.length);
+
+        client.query("insert into agendaprofessor (idprofessor, iddiasemana, idhorainicio, idhorafim) values ($1, $2," + 
             "(select idhorainicio from horainicio where descricaohorainicio = ($3)), " +
             "(select idhorafim from horafim where descricaohorafim = ($4)))"
-            , [data.nomeProfessor, data.descricaoDiaSemana, data.descricaoHoraInicio, data.descricaoHoraFim]);       
+            ,[data.idprofessor, diaSemanaString, data.descricaoHoraInicio[i], data.descricaoHoraFim[i]]);
+        res.send({
+            message: 'ok'
+        });   
     }
-    res.send({
-        message: 'ok'
-    });
 });
 module.exports = router;
