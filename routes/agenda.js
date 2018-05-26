2a42b47085ca3a92bf9b85ae7f41d9c779b9d9a7
Gabriel <gabriel_14_oliveira@hotmail.com>
2018-05-26 12:55:22 -0300

    Agenda

--- routes/agenda.js
+++ routes/agenda.js
@@ -2,93 +2,78 @@ var express = require('express');
 var router = express.Router();
 var client = require('./dbConnection');
 
-router.get('/listarpaciente', function(req, res, next) {
-    
-    client.query("SELECT  P.idpaciente, P.nomepaciente FROM paciente P INNER JOIN estagiariopacientes ep on ep.idpaciente = P.idpaciente WHERE P.idpaciente NOT IN (SELECT idpaciente FROM agenda) AND aprovado = 1 ORDER BY nomepaciente;", (err, response) => {
-      if (err) throw err;
-      res.send(response.rows);
-    });          
-  });
+router.get('/listarpaciente', function(req, res, next) {    
+  client.query("SELECT  P.idpaciente, P.nomepaciente"
+                + " FROM paciente P "
+                + " INNER JOIN estagiariopacientes ep on ep.idpaciente = P.idpaciente  "
+                + " WHERE P.idpaciente NOT IN (SELECT idpaciente FROM agenda LIMIT 1)  "
+                + " AND aprovado = 1 "
+                + " ORDER BY nomepaciente;", (err, response) => {
+    if (err) throw err;
+    res.send(response.rows);
+  });          
+});
 
-  router.post('/listarprofessor', function(req, res, next) {
-    const data = {idpaciente: req.body.paciente};
-    client.query(" SELECT ap.idprofessor FROM agendaprofessor ap "                  
-                  + " INNER JOIN professor p on p.idprofessor = ap.idprofessor "                
-                  + " INNER JOIN grupoestagiarios ge on ge.idestagio = p.idestagio "
-                  + " INNER JOIN estagiariopacientes ep on ep.idestagiario = ge.idestagiario "
-                  + " WHERE ep.idpaciente = $1 "
-                  + " AND ge.ativo = 1 ",[data.idpaciente], (err, response) => {
-      if (err) throw err;
-      res.send(response.rows);
-    });          
-  });
+router.post('/listarprofessor', function(req, res) {
+  const data = {idpaciente: req.body.paciente};
+  client.query(" SELECT DISTINCT P.idprofessor, P.nomeprofessor "
+                + " FROM professor P "
+                + " INNER JOIN grupoestagiarios GE on GE.idestagio = P.idestagio "
+                + " INNER JOIN estagiariopacientes EP on EP.idestagiario = GE.idestagiario "
+                + " WHERE EP.idpaciente = $1 "
+                + " AND GE.ativo = 1 "
+                + " AND P.ativo = 1 ",[data.idpaciente], (err, response) => {
+    if (err) throw err;
+    res.send(response.rows);
+  });          
+});
 
-  router.post('/listardia', function(req, res) {
+router.post('/listardia', function(req, res) {
+  const data = {idprofessor: req.body.idprofessor};
+  client.query(" SELECT DISTINCT DS.iddiasemana, DS.descricaosemana "
+              + "   FROM diasemana DS "
+              + " INNER JOIN agendaprofessor AP ON AP.iddiasemana = DS.iddiasemana "              
+              + " WHERE AP.idprofessor = $1 "
+              + " ORDER BY iddiasemana;", [data.idprofessor], (err, response) => {
+    if (err) throw err;
+    res.send(response.rows);
+  });          
+});
 
-    const data = {idpaciente: req.body.paciente};
-    client.query(" SELECT distinct ds.iddiasemana, ds.descricaosemana "
-                + "   from diasemana ds "
-                + " INNER JOIN agendaprofessor ap on ap.iddiasemana = ds.iddiasemana "
-                + " INNER JOIN professor p on p.idprofessor = ap.idprofessor "                
-                + " INNER JOIN grupoestagiarios ge on ge.idestagio = p.idestagio "
-                + " INNER JOIN estagiariopacientes ep on ep.idestagiario = ge.idestagiario "
-                + " WHERE ep.idpaciente = $1 "
-                + " AND ge.ativo = 1 "
-                + " ORDER BY iddiasemana;", [data.idpaciente], (err, response) => {
-      if (err) throw err;
-      res.send(response.rows);
-    });          
-  });
-
+router.post('/listarhorario', function(req, res) {  
+  const data = {idprofessor: req.body.professor, dia: req.body.dia};
   
-
-  router.post('/listarhorario', function(req, res, next) {
-
-    
-    const data = {idpaciente: req.body.paciente, idprofessor: req.body.professor[0].idprofessor, dia: req.body.dia};
-    console.log(data.dia);
-    
-    client.query("select hi.descricaohorainicio, hi.idhorainicio "
-                  + "from horainicio hi "
-                  + "where hi.descricaohorainicio BETWEEN ("
-                  + "    select DISTINCT hinicio.descricaohorainicio"
-                  + "    from horainicio hinicio"
-                  + "    inner join agendaprofessor ap on ap.idhorainicio = hinicio.idhorainicio "
-                  + "    INNER JOIN professor p on p.idprofessor = ap.idprofessor "                
-                  + "    INNER JOIN grupoestagiarios ge on ge.idestagio = p.idestagio "
-                  + "    INNER JOIN estagiariopacientes ep on ep.idestagiario = ge.idestagiario "
-                  + "    WHERE ap.idprofessor = $1"
-                  + "    AND ap.iddiasemana = $2 "
-                  + "    AND ge.ativo = 1 "
-                  + "    LIMIT 1"
-                  + "  ) AND ("
-                  + "    select DISTINCT hf.descricaohorafim "
-                  + "    from horafim hf "
-                  + "    inner join agendaprofessor ap on ap.idhorafim = hf.idhorafim "
-                  + "    INNER JOIN professor p on p.idprofessor = ap.idprofessor "                
-                  + "    INNER JOIN grupoestagiarios ge on ge.idestagio = p.idestagio "
-                  + "    INNER JOIN estagiariopacientes ep on ep.idestagiario = ge.idestagiario "
-                  + "    WHERE ap.idprofessor = $3"
-                  + "    AND ap.iddiasemana = $4 "
-                  + "    AND ge.ativo = 1 "
-                  + "    LIMIT 1 "                  
-                  + "  ) ORDER BY hi.descricaohorainicio", [data.idprofessor, data.dia, data.idprofessor, data.dia], (err, response) => {
-      if (err) throw err;
-      res.send(response.rows);
-    });          
-  });
+  client.query(" SELECT DISTINCT HI.descricaohorainicio, HI.idhorainicio "
+                + " FROM horainicio HI "
+                + " WHERE HI.descricaohorainicio BETWEEN ("
+                + "    SELECT DISTINCT hinicio.descricaohorainicio"
+                + "    FROM horainicio hinicio"
+                + "    INNER JOIN agendaprofessor ap ON ap.idhorainicio = hinicio.idhorainicio "
+                + "    WHERE ap.idprofessor = $1 "
+                + "    AND ap.iddiasemana = $2 "
+                + "    LIMIT 1"
+                + "  ) AND ("
+                + "    SELECT DISTINCT hf.descricaohorafim "
+                + "    FROM horafim hf "
+                + "    INNER JOIN agendaprofessor ap ON ap.idhorafim = hf.idhorafim "
+                + "    WHERE ap.idprofessor = $3 "
+                + "    AND ap.iddiasemana = $4 "
+                + "    LIMIT 1 "                  
+                + "  ) ORDER BY HI.descricaohorainicio", [data.idprofessor, data.dia, data.idprofessor, data.dia], (err, response) => {
+    if (err) throw err;
+    res.send(response.rows);
+  });          
+});
 
 router.post('/cadastrar', function(req, res){ 
   
   const data = {idpaciente: req.body.paciente, iddiasemana: req.body.dia, idhorainicio: req.body.horario, numerosessoes: req.body.numeroSessoes, datainicio: req.body.dataInicioAtendimento};
   
-    console.log(data);
-
-  client.query("INSERT INTO agenda(idpaciente, iddiasemana, idhorainicio, numerosessoes, datainicio) values($1, $2, $3, $4, $5)", [data.idpaciente, data.iddiasemana, data.idhorainicio, data.numerosessoes, data.datainicio]);         
-    res.send({
-      message: 'ok'
-    });
-   
+  client.query("INSERT INTO agenda (idpaciente, iddiasemana, idhorainicio, numerosessoes, datainicio) values($1, $2, $3, $4, $5) RETURNING codigo", [data.idpaciente, data.iddiasemana, data.idhorainicio, data.numerosessoes, data.datainicio],(err, response) => {
+    if (err) throw err;
+    console.log(response.rows);
+  });    
+  res.send({message: 'ok'});
 });
 
 //tamo testando
