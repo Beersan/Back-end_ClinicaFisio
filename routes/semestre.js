e00f774464753e1b6963096983f85f4d720f17e9
thaisdv <thaisdovalle94@hotmail.com>
2018-06-09 14:48:19 -0300

    alteração semestre

--- routes/semestre.js
+++ routes/semestre.js
@@ -3,13 +3,36 @@ var router = express.Router();
 var client = require('./dbConnection');
 
 router.post('/cadastrar', function(req, res){ 
-  
   const data = {nome: req.body.nomeSemestre, dataInicio: req.body.dataInicioSemestre, dataFim: req.body.dataFimSemestre};
-  client.query("INSERT INTO semestre(nomesemestre, datainiciosemestre, datafimsemestre) values($1, $2, $3)", [data.nome, data.dataInicio, data.dataFim]);         
+  
+  client.query('SELECT CASE WHEN ativo = 1 THEN 0 ELSE 1 END AS ativo FROM semestre ORDER BY ativo DESC LIMIT 1;', (err, response) => {
+    var ativo;
+    if (err) throw err;
+    console.log(response.rows);
+    if (response.rows[0] == null || response.rows[0] == ''){
+      ativo = "1";  
+    } else {
+      ativo = response.rows[0].ativo;
+    }
+    client.query("INSERT INTO semestre(nomesemestre, datainiciosemestre, datafimsemestre, ativo) values($1, $2, $3, $4)", [data.nome, data.dataInicio, data.dataFim, ativo]);         
     res.send({
       message: 'ok'
     });
+  });      
 });
+
+router.post('/alterarstatus', function(req, res){ 
+  
+  const data = {idSemestre: req.body.idSemestre};
+  
+  client.query("update semestre set ativo = 0 where idsemestre = (select idsemestre from semestre where ativo = 1)", (err, response) => {
+    client.query("update semestre set ativo = (select case when ativo = 1 then 0 else 1 end from semestre where idsemestre = $1) where idsemestre = $2", [data.idSemestre, data.idSemestre]);         
+    res.send({
+      message: 'ok'
+    });
+  });         
+});
+
 
 router.get('/listar', function(req, res, next) {
   
